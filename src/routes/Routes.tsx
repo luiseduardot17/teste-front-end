14888f21c332b6a43b1ccc029612192af4ff5780
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-01 19:02:37 -0300

    ✨ feat: add novas rotas ao código

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route } from 'react-router'
 import { BrowserRouter, Routes as Switch } from 'react-router-dom'
+import AdmCategorias from '../pages/Adm/AdmCategorias'
+import FormularioCategoria from '../pages/Adm/FormularioCategoria'
 import Categoria from '../pages/Categoria/Categoria'
 import Home from '../pages/Home/Home'
 
@@ -10,6 +12,9 @@ const Routes = () => {
     <Switch>
         <Route path='/' element={<Home/>}/>
         <Route path='/categoria' element={<Categoria/>}/>
+        <Route path='/admin/categoria' element={<AdmCategorias/>}/>
+        <Route path='/admin/categoria/novo' element={<FormularioCategoria/>}/>
+        <Route path='/admin/categoria/:id' element={<FormularioCategoria/>}/>
     </Switch>
     </BrowserRouter>
   )
