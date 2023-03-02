10d54b17d354ae39a5a3b566e29b03aebce850de
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-02 03:42:49 -0300

    ✨ feat: criado novas rotas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router'
 import { BrowserRouter, Routes as Switch } from 'react-router-dom'
-import AdmCategorias from '../pages/Adm/AdmCategorias'
-import FormularioCategoria from '../pages/Adm/FormularioCategoria'
+import AdmCategorias from '../pages/Adm/Categorias/AdmCategorias'
+import BaseAdmin from '../pages/Adm/BaseAdmin'
+import FormularioCategoria from '../pages/Adm/Categorias/FormularioCategoria'
 import Categoria from '../pages/Categoria/Categoria'
 import Home from '../pages/Home/Home'
+import AdmProdutos from '../pages/Adm/Produtos/AdmProdutos'
+import FormularioProduto from '../pages/Adm/Produtos/FormularioProduto'
 
 const Routes = () => {
   return (
@@ -12,9 +15,15 @@ const Routes = () => {
     <Switch>
         <Route path='/' element={<Home/>}/>
         <Route path='/categoria' element={<Categoria/>}/>
-        <Route path='/admin/categoria' element={<AdmCategorias/>}/>
-        <Route path='/admin/categoria/novo' element={<FormularioCategoria/>}/>
-        <Route path='/admin/categoria/:id' element={<FormularioCategoria/>}/>
+
+        <Route path='/admin' element={<BaseAdmin/>}>
+          <Route path='categoria' element={<AdmCategorias/>}/>
+          <Route path='categoria/novo' element={<FormularioCategoria/>}/>
+          <Route path='categoria/:id' element={<FormularioCategoria/>}/>
+          <Route path='produtos' element={<AdmProdutos/>}/>
+          <Route path='produtos/novo' element={<FormularioProduto/>}/>
+          <Route path='produtos/:id' element={<FormularioProduto/>}/>
+        </Route>
     </Switch>
     </BrowserRouter>
   )
