4d2866b2b365746042424a39e50fcfcfdc709a2a
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-04 18:50:47 -0300

    ✨ feat: add novos links e estilização

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,27 +2,36 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/" className="nav-link text-bg-primary">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/categoria" className="nav-link text-bg-primary">Categorias</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/admin/categoria" className="nav-link text-bg-primary">Administração</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-lg bg-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand"><i className="bi bi-car-front" style={{ color: "white", fontSize: 30 + "px" }}></i></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav gap-3">
+            <li className="nav-item mx-auto">
+              <Link to="/" className="nav-link bg-tertiary text-light p-0">Início</Link>
+            </li>
+            <li className="nav-item mx-auto">
+              <Link to="/categoria" className="nav-link bg-tertiary text-light p-0">Categorias</Link>
+            </li>
+            <li className="nav-item mx-auto">
+              <Link to="/admin/categoria" className="nav-link bg-tertiary text-light p-0">Administração</Link>
+            </li>
+            <li className="nav-item mx-auto">
+              <a className="nav-link bg-tertiary text-light p-0">Acompanhamento</a>
+            </li>
+            <li className="nav-item mx-auto">
+              <a className="nav-link bg-tertiary text-light p-0">Relatórios</a>
+            </li>
+            <li className="nav-item mx-auto">
+              <a className="nav-link bg-tertiary text-light p-0">Documentos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
