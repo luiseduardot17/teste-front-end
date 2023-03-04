37aa7867cf0c9f78e21a9f80880aa2f7e7e30ddd
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-04 18:59:55 -0300

    👌 style: estilização e novas linhas de código

--- src/components/ListaCategorias/Categoria/Categoria.tsx
+++ src/components/ListaCategorias/Categoria/Categoria.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react"
 import ICategoria from "../../../interfaces/ICategoria"
+import IProduto from "../../../interfaces/IProduto"
+import http from "../../../service/api"
+import Produto from "../Produto/Produto"
 
 interface CategoriaProps {
-    categoria: ICategoria
+  categoria: ICategoria
+  produto: IProduto
 }
-const Categoria = ({ categoria }: CategoriaProps) => {
+const Categoria = ({ categoria, produto }: CategoriaProps) => {
+
+  const [produtos, setProdutos] = useState<IProduto[]>()
+  
+  // useEffect(() => {
+  //   http.get<IProduto[]>(`Product/${produto.id}`)
+  //     .then(resposta => {
+  //       setProdutos(resposta.data)
+  //     })
+  // }, [categoria.id])
+
   return (
     <div>
-        <h3>{categoria.name}</h3>
-        <h3>{categoria.description}</h3>
+      <div className="container-sm bg-secondary">
+        <h3>ID {categoria.id} - {categoria.name}</h3>
+      </div>
+      <div>
+        {produtos?.map(item => <Produto produto={item} key={item.id} />)}
+      </div>
     </div>
   )
 }
