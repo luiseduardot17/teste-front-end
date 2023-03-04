cc8a40412eaea937a3e2af2791aad1b2f539d812
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-04 18:55:02 -0300

    ✨ feat: add nova section e formatação do código

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import S from './Home.module.css'
+import cars from '../../assets/images/cars.png'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
-      Home
-      </div>
+    <div className={S.container}>
+      <Navbar />
+      <section className={S.section}>
+        <div><h1 className="display-1">Bem vindo!</h1></div>
+        <div><img src={cars} className="img-fluid" alt="icones de carros" /></div>
+      </section>
+    </div>
   )
 }
 
