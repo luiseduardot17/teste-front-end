44bf634737d90a49598ddd1a949697bb60f9c74d
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-04 18:58:37 -0300

    👌 style: pequenas mudanças na estilização

--- src/pages/Adm/Categorias/AdmCategorias.tsx
+++ src/pages/Adm/Categorias/AdmCategorias.tsx
@@ -12,41 +12,42 @@ const AdmCategorias = () => {
       .then(resposta => setCategorias(resposta.data))
   }, [])
 
+
   const excluir = (categoriaQueSeraExcluida: ICategoria) => {
-      http.delete(`ProductCategory/${categoriaQueSeraExcluida.id}`)
-      .then(()=> {
+    http.delete(`ProductCategory/${categoriaQueSeraExcluida.id}`)
+      .then(() => {
         alert("Categoria excluida com sucesso!")
         const listaCategoria = categorias.filter(categoria => categoria.id !== categoriaQueSeraExcluida.id)
-        setCategorias([ ...listaCategoria ])
+        setCategorias([...listaCategoria])
       })
       .catch(erro => console.log(erro))
   }
 
   return (
     <div>
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Nome da Categoria</th>
-          <th scope="col">Editar</th>
-          <th scope="col">Excluir</th>
-        </tr>
-      </thead>
-      <tbody>
+      <table className="table text-center">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome da Categoria</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
 
-        {categorias.map(categoria => {
-          return (
-            <tr key={categoria.id}>
-              <td>{categoria.id}</td>
-              <td>{categoria.name}</td>
-              <td><Link to={`/admin/categoria/${categoria.id}`} style={{textDecoration: "none"}}><button type="button" className="btn btn-outline-primary fw-bold"><i className="bi bi-pencil-square" ></i> Editar</button></Link></td>
-              <td><button type="button" className="btn btn-outline-danger fw-bold" onClick={()=> excluir(categoria)}><i className="bi bi-trash" ></i> Excluir</button></td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          {categorias.map(categoria => {
+            return (
+              <tr key={categoria.id}>
+                <td>{categoria.id}</td>
+                <td>{categoria.name}</td>
+                <td><Link to={`/admin/categoria/${categoria.id}`} style={{ textDecoration: "none" }}><button type="button" className="btn btn-outline-primary fw-bold"><i className="bi bi-pencil-square" ></i> Editar</button></Link></td>
+                <td><button type="button" className="btn btn-outline-danger fw-bold" onClick={() => excluir(categoria)}><i className="bi bi-trash" ></i> Excluir</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
