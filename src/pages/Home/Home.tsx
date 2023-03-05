1581faaff91fc98b591b27db89af7d367347a8bb
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-05 19:34:29 -0300

    👌 style: criado novo paragrafo e add icone

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import S from './Home.module.css'
 import cars from '../../assets/images/cars.png'
@@ -8,7 +7,8 @@ const Home = () => {
     <div className={S.container}>
       <Navbar />
       <section className={S.section}>
-        <div><h1 className="display-1">Bem vindo!</h1></div>
+        <div className={S.titulo}><h1 className="display-1 text-center">Bem vindo <i className="bi bi-car-front" style={{ fontSize: 60 + "px" }}></i></h1>
+          <p className="h2 text-center">Um sistema completo para controlar sua concessionária. Com ele você pode alcançar o máximo de produtividade em seus processos.</p></div>
         <div><img src={cars} className="img-fluid" alt="icones de carros" /></div>
       </section>
     </div>
