ed23d564efcc0a87ad0e955f9a29b127088e0cd5
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-05 19:41:31 -0300

    ✨ feat: add spinner ao carregamento da pagina

--- src/components/ListaCategorias/ListaCategorias.tsx
+++ src/components/ListaCategorias/ListaCategorias.tsx
@@ -6,18 +6,32 @@ import Categoria from './Categoria/Categoria'
 const ListaCategorias = () => {
 
   const [categorias, setCategorias] = useState<ICategoria[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     http.get('ProductCategory')
       .then(resposta => {
         setCategorias(resposta.data)
+        setLoading(true)
       })
       .catch(erro => {
         console.log(erro);
       })
   }, [])
+
   return (
     <section>
+      {categorias.length == 0 && loading == false &&
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>}
+      {categorias.length == 0 && loading == true && (
+        <div className="d-flex justify-content-center text-center w-100">
+          <h5>Nenhum resultado encontrado!</h5>
+        </div>
+      )}
       {categorias?.map(item => <Categoria categoria={item} key={item.id} />)}
     </section>
   )
